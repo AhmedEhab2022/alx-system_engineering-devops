1f87e47125ba2f4ac06f966fd630733e26f32467
root <am8122004@gmail.com>
2023-11-29 11:36:25 -0800

    100-puppet_ssh_config.pp

---
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -0,0 +1,13 @@
+# using Puppet to make changes to our configuration file
+
+file_line { 'Refuse to authenticate using a password':
+  ensure  => present,
+  path    => '~/.ssh/config',
+  line    => 'PasswordAuthentication no',
+}
+
+file_line { 'Use private key':
+  ensure  => present,
+  path    => '~/.ssh/config',
+  line    => 'IdentityFile ~/.ssh/school',
+}
