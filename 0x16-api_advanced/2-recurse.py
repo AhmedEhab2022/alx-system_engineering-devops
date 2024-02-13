2d6c8e39d5e22638fc1d575ea95db029bef49df0
AhmedEhab2022 <am8122004@gmail.com>
2024-02-13 11:22:44 -0800

    2-recurse.py

---
+++ 0x16-api_advanced/2-recurse.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""
+2-recurse.py
+"""
+import requests
+
+
+def recurse(subreddit, hot_list=[], i=0, posts=[]):
+    """
+    returns a list containing the titles of all hot articles for
+    a given subreddit. If no results are found for the given
+    subreddit, the function should return None.
+    """
+    if i == 0:
+        if subreddit is None:
+            return None
+        url = f'https://www.reddit.com/r/{subreddit}/hot.json'
+        res = requests.get(url, allow_redirects=False)
+        if res.status_code != 200:
+            return None
+        posts = res.json().get('data').get('children')
+    if i == len(posts):
+        return hot_list
+    hot_list.append(posts[i].get('data').get('title'))
+    recurse(subreddit, hot_list, i + 1, posts)
+    return hot_list
