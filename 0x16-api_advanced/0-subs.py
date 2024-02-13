9044d9efc5fb0d7cd0deb22613128ca636106263
AhmedEhab2022 <am8122004@gmail.com>
2024-02-13 13:40:51 -0800

    add user-agent

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -14,7 +14,7 @@ def number_of_subscribers(subreddit):
     if subreddit:
         url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
         headers = { 
-            'User-Agent': 'linux:com.api.advanced:v1.2.3 (by /u/Ahmed)'
+            'User-agent': 'ubuntu 20.04'
         }
         res = requests.get(url, headers=headers, allow_redirects=False)
         if res.status_code == 200:
